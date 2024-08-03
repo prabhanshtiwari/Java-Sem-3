c2fd212edd5e71e2d527d22fdb16f771f4bd003a
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-03 12:29:54 +0530

    Variables in Java

---
+++ 5 Variables in Java/first.java
@@ -0,0 +1,27 @@
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Program begins here");
+
+        System.out.println(3+5+6);
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
+
+/*
+ *We are trying to solve the real world problems using virtual world 
+ * To store data, we need database 
+ * 
+ * During processing, we need to save data somewhere. And that somewhere is called Variable
+ * variable -> box that store data
+ * 
+ * Java is strongly typed language, we need to specify datatype of each variable.
+ * String - for text
+ * int - for numbers
+ * 
+ * We can also add values and variables
+ */
