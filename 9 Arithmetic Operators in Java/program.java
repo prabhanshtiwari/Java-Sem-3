31bc932cab6316574d44a25d8694d438e8944aef
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-04 22:20:11 +0530

    Arithmetic operators in java

---
+++ 9 Arithmetic Operators in Java/program.java
@@ -0,0 +1,32 @@
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Program begins here");
+
+        int num1 = 7;
+        // int num2 = 5;
+        // int result = num1 % num2;
+
+        // num1 = num1 + 2;
+        num1 += 2;
+        System.out.println(num1);
+        
+    }
+}
+/*
+ * Arithmetic Operators: +, -, *, /, %, 
+ * Some Shorthands: 
+ * a = a + b is same as a += b
+ * a = a - b is same as a -= b
+ * a = a * b is same as a *= b
+ * a = a / b is same as a /= b
+ * a = a % b is same as a %= b
+ * 
+ * num += 1 is same as num++ // increment 
+ * num -= 1 is same as num-- // decrement 
+ * 
+ * ++num vs num++ ?
+ * ++num -> pre-increment -> increment and then assign
+ * num++ -> post-increment -> assign and then increment
+ * 
+ */
+
