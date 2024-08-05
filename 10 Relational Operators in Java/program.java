390c2d1c24cb6c1750555e6fc82cef06a9e3b9cb
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-05 07:29:56 +0530

    Relational Operators in Java

---
+++ 10 Relational Operators in Java/program.java
@@ -0,0 +1,31 @@
+
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Program begins here");
+
+        // int x = 6;
+        // int y = 5;
+
+        // boolean result = x < y;
+        // System.out.println(result);
+
+        // int x = 6;
+        // int y = 6;
+
+        // boolean result = x == y;
+        // System.out.println(result);
+
+
+
+
+    }
+}
+/*
+ *Relational Operators : used for comparison : >, >=, <, <=, ==, !=
+ * It gives boolean value as a result, either true or false
+ * 
+ * If we have two conditions, we combine those conditions with logocal operators(AND, OR, NOT)
+ * 
+ * 
+ * 
+ */
