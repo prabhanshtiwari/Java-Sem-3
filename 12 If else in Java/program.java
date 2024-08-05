d09604e59dad7563926fe9bac3c80afc91c199ff
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-05 08:16:27 +0530

    if else in java

---
+++ 12 If else in Java/program.java
@@ -0,0 +1,48 @@
+
+
+public class program {
+    public static void main(String args[]){
+        System.out.println("Program begins here");
+
+        // int x = 5;
+        // int y = 7;
+        // // if block
+
+        // if(x > 10){
+        //     System.out.println("Hello");
+        // }
+
+        // if else block
+
+        // if(x > 10 && x <= 20) {     // 11 - 20
+        //     System.out.println("Hello");
+        // }else {
+        //     System.out.println("Bye");
+        // }
+
+        int x = 5;
+        int y = 7;
+
+        if (x > y) {
+            System.out.println(x);
+            System.out.println("Thank You");
+        }else {
+            System.out.println(y);
+        }
+        
+        
+        
+        
+    }
+}
+/*
+ * If you have only one statement in if or else block then we don't need to have braces
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
