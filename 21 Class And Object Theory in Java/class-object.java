0c608c78b31881b944f157c6f77ef4dced596247
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-06 22:30:23 +0530

    class and object theory in java

---
+++ 21 Class And Object Theory in Java/class-object.java
@@ -0,0 +1,24 @@
+
+
+public class class-object {
+    
+    public static void main(String args[]){
+
+
+
+
+    }
+}
+/*
+ * Object: A real world entity
+ * This Object has properties and behaviour
+ * 
+ * To create an object, we need to create a class first
+ * Class -> it is a blueprint to create an object
+ * 
+ * 
+ * Who creates the object in Java? -> Java Virtual Machine
+ * 
+ * 
+ * 
+ */
