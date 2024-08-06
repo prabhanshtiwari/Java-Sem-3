143902daeccbb0166b49102a1b526c0917c03774
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-06 17:26:38 +0530

    Need for loops in Java

---
+++ 16 Need for loop in Java/need.java
@@ -0,0 +1,24 @@
+
+public class need {
+    public static void main(String args[]){
+        
+
+        {   
+            System.out.println("Hi");
+        }
+
+
+
+
+    }
+}
+/*
+ * Need of Loop:
+ * 1. To do any task repeatedly
+ * 
+ * loops: while, do-while, for
+ * 
+ * 
+ * 
+ * 
+ */
