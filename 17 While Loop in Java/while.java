99e5d19fdc0da4c8d5e91d5a508e4aec7658b9be
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-06 17:49:58 +0530

    While Loop in Java

---
+++ 17 While Loop in Java/while.java
@@ -0,0 +1,27 @@
+public class While {
+    public static void main(String args[]){
+        
+        int i = 1;
+        while (i <= 4){
+            System.out.println("Hi "+ i);
+            int j = 1;
+            while (j <= 3) { 
+                System.out.println("Hello "+ j );
+                j++;
+            }
+
+
+            i++;
+        }
+
+        System.out.println("Bye"+i);
+
+    }
+}
+/*
+ * While Loop in Java:
+ * 
+ * 
+ * Nested Loop: Inner loop & outer loop
+ * 
+ */
