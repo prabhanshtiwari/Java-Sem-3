e32f9110795db8a971f5b90076002cbde1592416
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-06 21:41:21 +0530

    do-while loop in java

---
+++ 18 Do While Loop in Java/dowhile.java
@@ -0,0 +1,24 @@
+
+public class dowhile {
+    public static void main(String args[]){
+        
+
+        int i = 1;
+
+        do
+        {
+            System.out.println("Hi" + i);
+            i++;
+        }while(i <= 4);
+
+        
+
+
+
+    }
+}
+/*
+ *Do-While Loop :
+ * It will execute the block atleast once
+ * 
+ */
