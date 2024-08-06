d3adadad5282690c3a0656c09ba76b8f39c257d5
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-06 10:42:35 +0530

    Ternary Operator in Java

---
+++ 14 Ternary Operator in Java/program.java
@@ -0,0 +1,30 @@
+
+public class program {
+    public static void main(String args[]){
+        System.out.println("Program begins here");
+
+        //to check if the given number is even or odd
+        int num = 4;
+        int result;
+        // if (num % 2 == 0){
+        //     result = 10;
+        // }else {
+        //     result = 20;
+        // }
+
+        // ? : 
+        result = num % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
+
+
+
+    }
+}
+/*
+ * Ternary Operator - Used to write if else in one line
+ * 
+ * 
+ * 
+ * 
+ */
