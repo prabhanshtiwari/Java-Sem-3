f5de78604b338dcedef7c65f41e4b9d41a021b5b
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2024-08-06 10:28:19 +0530

    If Else If in Java

---
+++ 13 If Else If in Java/program.java
@@ -0,0 +1,26 @@
+
+public class program {
+    public static void main(String args[]){
+        System.out.println("Program begins here");
+
+        int x = 8;
+        int y = 17;
+        int z = 9;
+        
+        // Combining two conditions
+        if (x > y && x > z){
+            System.out.println(x);
+            
+        }else if(y > z) {
+            System.out.println(y);
+        }
+        else {
+            System.out.println(z);
+        }
+    }
+}
+/*
+ * if-else if-else
+ * 
+ * 
+ */
